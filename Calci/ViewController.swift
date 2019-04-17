e91eefd94a161f911f116ef86398dfed30348641
SD05 <35373111+SD05@users.noreply.github.com>
2019-04-17 14:32:12 +0530

    Add files via upload

---
+++ Calci/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Shubham Kumar on 07/04/19.
+//  Copyright © 2019 Shubham Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
